d3c7cc93b395b5865e0b8b61c872ddfcf65979c8
Louis ROUBY <110021874+Laroub10@users.noreply.github.com>
2023-02-04 09:44:45 +0100

    Update get_gcp_data.py

--- foodE/get_gcp_data.py
+++ foodE/get_gcp_data.py
@@ -7,16 +7,18 @@ def get_gcp_data():
 
     bucket_name = os.environ.get('BUCKETNAME')
     filename = os.environ.get('FILENAME')
-    #start_byte = 0
-    # end_byte = 1048576 # 1 MB
 
     source = "gs://" + bucket_name + "/" + filename
-    destination = "../code/wroby/foodE/raw_data"
+    
+    if os.environ.get("DESTINATION") == "local":
+        destination = "../code/wroby/foodE/raw_data/food-101"
+    elif os.environ.get("DESTINATION") == "cloud":
+        os.mkdir("/FoodE/raw_data")
+        destination = "./FoodE/raw_data"
+    else: print("No source selected")
 
 
     # Use gsutil cp with the -r (range) flag to download a portion of the file
     result = subprocess.run(["gsutil", "cp", "-r","-m",  source, destination], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-
-    #f"{start_byte}-{end_byte}" -> pas sûr de l'ajout ?
 
     return result
