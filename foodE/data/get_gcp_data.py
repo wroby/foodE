92ca5ee394bb2b505e7065a42c560f1bd7a92e27
wroby <twrobel77@gmail.com>
2023-02-04 12:52:34 +0100

    Update get_gcp_data.py

--- foodE/data/get_gcp_data.py
+++ foodE/data/get_gcp_data.py
@@ -9,21 +9,19 @@ def get_gcp_data():
     filename = os.environ.get('FILENAME')
 
     source = "gs://" + bucket_name + "/" + filename
-    print(source)
 
     if os.environ.get("DESTINATION") == "local":
         file_path = os.path.abspath(__file__)
         grandparent_folder = os.path.dirname(os.path.dirname(os.path.dirname(file_path)))
         destination = os.path.join(grandparent_folder, "raw_data/")
-        print(destination)
+        
     elif os.environ.get("DESTINATION") == "cloud":
         os.mkdir("/FoodE/raw_data")
         destination = "./FoodE/raw_data/"
+        
     else: print("No source selected")
-
-
 
     # Use gsutil cp with the -r (range) flag to download a portion of the file
     result = subprocess.run(["gsutil" ,"-m","cp", "-r",  source, destination])
-    print(result)
+    
     return result
