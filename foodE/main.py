51cf9049280659d35aa9c13a5080d9b30e310b3b
Léo Graziani <103998540+GrGLeo@users.noreply.github.com>
2023-02-07 22:17:35 +0100

    mlflow logic (#23)
    * mlflow logic
    
    * petite erreur
    
    * change env name

--- foodE/main.py
+++ foodE/main.py
@@ -5,6 +5,7 @@ import os
 from foodE.local_data import get_local_data
 from foodE.preprocessor import preprocessing
 from foodE.model import initialize_model, compiler, fitting, eval
+from foodE.registery import save_model
 
 # def get_data():
 #     train, validation, test = get_local_data()
@@ -25,9 +26,26 @@ def training():
     model = compiler(model)
     print("compiled model")
     model,history = fitting(model, train=train , validation = validation )
-    eval(model,test)
-    results = model.evaluate(test, verbose=1)
-    print(f"Test Accuracy: {results[1] * 100:.2f}%")
+
+    #Get lowest val_accuracy
+    val_acc = np.min(history.history['val_accuracy'])
+
+    #Params to load to mlflow
+    params = dict(
+    # Model parameters
+    learning_rate=os.getenv("LEARNING_RATE"),
+    batch_size=os.getenv("BATCH_SIZE "),
+    epochs = os.getenv("EPOCH"),
+    regularizerl1 = os.getenv("REGULARIZER_L1"),
+    regularizerl2 = os.getenv("REGULARIZER_L2"),
+    patience=os.getenv("PATIENCE "),
+    context="train")
+
+    save_model(model = model, params = params, metrics = val_acc)
+
+    # eval(model,test)
+    # results = model.evaluate(test, verbose=1)
+    # print(f"Test Accuracy: {results[1] * 100:.2f}%")
     return None
 
 
---
+++ foodE/registery.py
@@ -0,0 +1,42 @@
+import mlflow
+from mlflow.tracking import MlflowClient
+
+import os
+
+
+from tensorflow import keras
+
+
+def save_model(model: keras.Model = None,
+               params: dict = None,
+               metrics: dict = None) -> None:
+    """
+    persist trained model, params and metrics
+    """
+
+    if os.getenv("MODEL_TARGET") == "mlflow":
+
+        # retrieve mlflow env params
+        track = os.getenv("MLFLOW_TRACKING_URI")
+        experiment = os.getenv("MLFLOW_EXPERIMENT")
+
+        # configure mlflow
+        mlflow.set_tracking_uri(track)
+        mlflow.set_experiment(experiment_name=experiment)
+
+        with mlflow.start_run():
+
+            # STEP 1: push parameters to mlflow
+            mlflow.log_params(params)
+
+            # STEP 2: push metrics to mlflow
+            mlflow.log_metrics(metrics)
+
+            # STEP 3: push model to mlflow
+            if model:
+                mlflow.keras.log_model(keras_model=model,
+                            artifact_path="model",
+                            keras_module="tensorflow.keras",
+                            registered_model_name="foodE_model")
+
+        return None
