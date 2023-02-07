93b8783345c014883e2937f50c6bc5e748fc9d8e
leo graziani <graziani.gothe.leo@gmail.com>
2023-02-07 21:59:27 +0100

    mlflow logic

--- foodE/main.py
+++ foodE/main.py
@@ -5,6 +5,7 @@ import os
 from foodE.local_data import get_local_data
 from foodE.preprocessor import preprocessing
 from foodE.model import initialize_model, compiler, fitting, eval
+from foodE.registery import save_model
 
 # def get_data():
 #     train, validation, test = get_local_data()
@@ -20,11 +21,27 @@ def training():
     train,validation,test = get_local_data()
     train, validation, test = preprocessing(train,validation, test)
     print(f"\n✅ Data processed entirely")
-    model = initialize_model()
+    model = initialize_model(img_height=64,img_width=64)
     print("initialized model")
     model = compiler(model)
     print("compiled model")
     model,history = fitting(model, train=train , validation = validation )
+
+    #Get lowest val_accuracy
+    val_acc = np.min(history.history['val_accuracy'])
+
+    #Params to load to mlflow
+    params = dict(
+    # Model parameters
+    learning_rate=os.getenv("learning_rate"),
+    batch_size=os.getenv("batch_size"),
+    patience=os.getenv("patience"),
+    context="train")
+
+    save_model(model = model, params = params, metrics = val_acc)
+
+
+
     eval(model,test)
     results = model.evaluate(test, verbose=1)
     print(f"Test Accuracy: {results[1] * 100:.2f}%")
