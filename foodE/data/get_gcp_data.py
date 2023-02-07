d524ba84b322c11e6be89dbb503e5c1745c87aec
Thomas Wrobel <twrobel77@gmail.com>
2023-02-07 21:00:50 +0100

    download train and test

--- foodE/data/get_gcp_data.py
+++ foodE/data/get_gcp_data.py
@@ -6,22 +6,21 @@ import os
 def get_gcp_data():
 
     bucket_name = os.environ.get('BUCKETNAME')
-    filename = os.environ.get('FILENAME')
+    #filename = os.environ.get('FILENAME')
 
-    source = "gs://" + bucket_name + "/" + filename
-
+    source = "gs://" + bucket_name + "/*"
     if os.environ.get("DESTINATION") == "local":
         file_path = os.path.abspath(__file__)
         grandparent_folder = os.path.dirname(os.path.dirname(os.path.dirname(file_path)))
         destination = os.path.join(grandparent_folder, "raw_data/")
-        
+
     elif os.environ.get("DESTINATION") == "cloud":
         os.mkdir("/FoodE/raw_data")
         destination = "./FoodE/raw_data/"
-        
+
     else: print("No source selected")
 
     # Use gsutil cp with the -r (range) flag to download a portion of the file
     result = subprocess.run(["gsutil" ,"-m","cp", "-r",  source, destination])
-    
+
     return result
