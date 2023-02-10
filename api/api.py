89f04e6e46c278471aff828815811cbaddd5bb42
Louis ROUBY <louis.rouby1@gmail.com>
2023-02-10 23:52:18 +0100

    api_key modif

--- api/api.py
+++ api/api.py
@@ -2,9 +2,10 @@ import os
 import requests
 
 #params
-recipe = os.environ.get('prediction')
+recipe = os.environ.get('PREDICTION')
 #possible querys
-
+#API KEY
+key = os.environ.get('API_KEY')
 
 #Get complex search
 #URL = "https://api.spoonacular.com/recipes/complexSearch?"
@@ -13,7 +14,7 @@ recipe = os.environ.get('prediction')
 #URL = "https://api.spoonacular.com/recipes/findByNutrients"
 
 def get_recipe_instructions(recipe):
-    url = f"https://api.spoonacular.com/{recipe}/complexSearch?"
+    url = f"https://api.spoonacular.com/{recipe}/complexSearch?/{key}"
     response = requests.get(url)
     if response.status_code == 200:
         return response.json()
