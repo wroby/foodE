9dff4da5cff5cb93d389e17d1a7eb58a4d347128
Constantino Baboun Garib <112987013+ConsBabG@users.noreply.github.com>
2023-02-11 13:59:28 +0100

    Custom model (#33)
    * custom model
    
    * new
    
    * custom

---
+++ foodE/custom_model.py
@@ -0,0 +1,20 @@
+from tensorflow.keras import models
+from tensorflow.keras import Sequential, layers
+import os
+
+
+#Architecture
+
+def custom_model(img_height, img_width):
+
+    model = models.Sequential()
+    model.add(layers.Conv2D(32, (3, 3), input_shape=(img_height, img_width, 3), activation = 'relu'))
+    model.add(layers.MaxPooling2D(pool_size=(2, 2)))
+
+    model.add(layers.Conv2D(32, (3, 3), activation = 'relu'))
+    model.add(layers.MaxPooling2D(pool_size=(2, 2)))
+
+    model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+    model.add(layers.MaxPooling2D(pool_size=(2, 2)))
+
+    return model
