1134fb9481d55217bd05642d2cb4d1fd9cd007e1
Louis ROUBY <louis.rouby1@gmail.com>
2023-02-11 13:05:02 +0100

    api get_nutrients func

--- api/api.py
+++ api/api.py
@@ -1,21 +1,29 @@
 import os
 import requests
 
-#params
-
 #API KEY
+
 key = os.environ.get('API_KEY')
 
-#Get complex search
-#URL = "https://api.spoonacular.com/recipes/complexSearch?"
 
-#Get findByNutrients
-#URL = "https://api.spoonacular.com/recipes/findByNutrients"
-
-def get_recipe_instructions(recipe):
-    url = f"https://api.spoonacular.com/{recipe}/complexSearch?/{key}"
-    response = requests.get(url)
+def get_macronutrients(recipe):
+    if recipe == "cup_cakes":
+        recipe = recipe.replace("cup_cakes","cupcakes")
+    elif recipe == "french_fries":
+        recipe = recipe.replace("french_fries","fries_french")
+    elif recipe == "fried_rice":
+        recipe = recipe.replace("fried_rice","rice_fried")
+    elif recipe == "fried_calamari":
+        recipe = recipe.replace("fried_calamari","calamari_fried")
+    elif recipe == "hot_and_sour_soup":
+        recipe = recipe.replace("hot_and_sour_soup","sour_soup")
+    elif recipe == "hot_dog":
+        recipe = recipe.replace("hot_dog","hotdog")
+    elif recipe == "prime_rib":
+        recipe = recipe.replace("prime_rib","rib_prime")
+    else:
+        response = requests.get("https://api.spoonacular.com/recipes/guessNutrition?apiKey={key}&title={recipe}").json()
     if response.status_code == 200:
-        return response.json()
+        return response
     else:
         return None
