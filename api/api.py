2d2b98c5088ba56d1bb878264687f09890e0c11b
Louis ROUBY <110021874+Laroub10@users.noreply.github.com>
2023-02-11 09:26:11 +0100

    Louis (#30)
    * main.py
    
    * gitignore
    
    * help
    
    * work on api
    
    * api_key modif
    
    * api_key modif

---
+++ api/api.py
@@ -0,0 +1,21 @@
+import os
+import requests
+
+#params
+
+#API KEY
+key = os.environ.get('API_KEY')
+
+#Get complex search
+#URL = "https://api.spoonacular.com/recipes/complexSearch?"
+
+#Get findByNutrients
+#URL = "https://api.spoonacular.com/recipes/findByNutrients"
+
+def get_recipe_instructions(recipe):
+    url = f"https://api.spoonacular.com/{recipe}/complexSearch?/{key}"
+    response = requests.get(url)
+    if response.status_code == 200:
+        return response.json()
+    else:
+        return None
