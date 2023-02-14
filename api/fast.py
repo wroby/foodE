e1974f4395a89d436b4964b33d154a877b910720
wroby <twrobel77@gmail.com>
2023-02-14 19:14:34 +0100

    Update fast.py

--- api/fast.py
+++ api/fast.py
@@ -1,20 +1,19 @@
 from fastapi import FastAPI
-from foodE.registery import load_model
+from foodE.registery import model_load
 import numpy as np
-from foodE.registery import save_model
 import mlflow.keras
 from tensorflow import keras
 
 app = FastAPI()
 
-app.state.model = load_model()
+app.state.model = model_load()
 
 @app.get("/predict")
 
 def prediction(img):
     #Check input type
     model = app.state.model
-    pred = model.predict(img)
+    pred = model.predict(img) #img = image tensor
     idx = np.argmax(pred)
     recipe = classes[idx]
-    return recipe
+    return recipe 
