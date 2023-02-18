2d8eb411d5dfe0a2eabc7f52433c3d04f87bd84f
Louis ROUBY <110021874+Laroub10@users.noreply.github.com>
2023-02-18 14:50:27 +0100

    connect api to BQ (#48)

--- api/api.py
+++ api/api.py
@@ -21,9 +21,11 @@ def get_macronutrients(recipe):
         recipe = recipe.replace("hot_dog","hotdog")
     elif recipe == "prime_rib":
         recipe = recipe.replace("prime_rib","rib_prime")
-    response = requests.get("https://api.spoonacular.com/recipes/guessNutrition?apiKey={key}&title={recipe}")
+    response = requests.get(f"https://api.spoonacular.com/recipes/guessNutrition?apiKey={key}&title={recipe}")
     if response.status_code == 200:
         return response.json()
+
+
     else:
         print(response.status_code)
         return None
