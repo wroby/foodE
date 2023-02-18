62e8b3c31a85330de4faf0265d465162a2d4d04f
wroby <twrobel77@gmail.com>
2023-02-18 09:37:06 +0100

    Update registery.py

--- foodE/registery.py
+++ foodE/registery.py
@@ -13,7 +13,7 @@ file_path = os.path.abspath(__file__)
 LOCAL_PATH = os.path.join(os.path.dirname(file_path),"models")
 model_path = os.path.join(LOCAL_PATH,f"{os.getenv('MODEL')}_{timestamp}")
 
-def save_model(model = None, 
+def save_model(model = None,
                params: dict = None,
                metrics: dict = None,) -> None:
     """
