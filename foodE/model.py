e39a7f16cc8cf837f9c3f8286c1c70b4c718e4d4
Léo Graziani <103998540+GrGLeo@users.noreply.github.com>
2023-02-28 19:21:45 +0100

    Load h5 model and good prediction (#58)

--- foodE/model.py
+++ foodE/model.py
@@ -166,7 +166,7 @@ def eval(model,test):
 def pred(model, img):
     #Checkez le type(img) pour le predict
     path = os.path.join(os.path.dirname(os.path.dirname(__file__)),"raw_data/food-101/food-101-test/images")
-    classes = [x for x in os.listdir(path) if os.path.isdir(os.path.join(path,x))]
+    classes = sorted([x for x in os.listdir(path) if os.path.isdir(os.path.join(path,x))])
     results = model.predict(img)
     idx = np.argmax(results)
     recipe = classes[idx]
